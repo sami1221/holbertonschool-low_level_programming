b0b65bd13375b2ae69403149beb6f2cbd470b7ef
sami1221 <samarabts2004@gmail.com>
2022-09-29 10:59:14 -0700

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 if (n > 0)
 {
-	printf("is positive\n");
+	printf("%i is positive\n");
 }
 else if (n == 0)
 {
-	printf("is zero\n");
+	printf("%i is zero\n");
 }
 else
 {
-	printf("is negative\n");
+	printf("%i is negative\n");
 }
 return (0);
 }
