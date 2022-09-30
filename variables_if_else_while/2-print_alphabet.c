10f422df2500ebf847ff5458f8d02c41987254da
sami1221 <samarabts2004@gmail.com>
2022-09-30 07:06:14 -0700

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - no comment
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char chars;
+
+	for (chars = 97; chars <= 122; chars++)
+	putchar(chars);
+	putchar('\n');
+	return (0);
+}
