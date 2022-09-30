e9d328c36a8f5d0392f6f4c8029f799bf7a182b0
sami1221 <samarabts2004@gmail.com>
2022-09-30 09:41:59 -0700

    print base

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - no comment
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = '0';
+	char chars;
+
+	for (; num <= '9'; num++)
+		putchar(num);
+	for (chars = 97; chars <= 102; chars++)
+		putchar(chars);
+	putchar('\n');
+	return (0);
+}
