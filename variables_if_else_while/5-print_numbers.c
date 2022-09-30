f26c264824f69095ba37e2a3128f48736b567f21
sami1221 <samarabts2004@gmail.com>
2022-09-30 07:23:48 -0700

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ * main - no comment
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+		printf("%d", num);
+	putchar ('\n');
+	return (0);
+}
