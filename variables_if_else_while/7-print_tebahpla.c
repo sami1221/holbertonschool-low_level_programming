606bf005c73b073387f094fe13e1cf4e123f6fc9
sami1221 <samarabts2004@gmail.com>
2022-09-30 09:33:57 -0700

    print tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - no comment
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 122; rev >= 97; rev--)
+		putchar(rev);
+	putchar('\n');
+	return (0);
+}
