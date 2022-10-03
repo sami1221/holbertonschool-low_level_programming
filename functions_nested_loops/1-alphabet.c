dd62f494b3d69dbe095101e526c2870d1ea5a741
sami1221 <samarabts2004@gmail.com>
2022-10-03 06:33:22 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 97; alph <= 122; alph++)
+	{		_putchar(alph);
+			_putchar(10);
+	}
+}
