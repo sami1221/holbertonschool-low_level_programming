87535d18b202656421bf3a6455cf36af541e0075
sami1221 <samarabts2004@gmail.com>
2022-10-05 14:16:46 -0700

    fix betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,28 +7,29 @@
 int main(void)
 {
 	int i;
-	for (i=1; i<=100; i++)
+
+	for (i = 1; i <= 100; i++)
 	{
-		if (i%15 == 0)
+		if (i % 15 == 0)
 		{
-			printf ("FizzBuzz");
+			printf("FizzBuzz");
 		}
-		else if ((i%3) == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		else if ((i%5) == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
 		else
 		{
 			printf("%i", i);
-		}	
+		}
 		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
 
