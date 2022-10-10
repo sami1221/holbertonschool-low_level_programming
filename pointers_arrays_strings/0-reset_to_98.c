f605e463bb5172288ac4d8dae7af9b32c03d2b60
sami1221 <samarabts2004@gmail.com>
2022-10-10 09:03:05 -0700

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value to 98
+ * @n: new pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;	/* operator * obtains the variable to which its operand points */
+}
