a292a5a66ca55789d2c601f61917c0ec5d500027
sami1221 <samarabts2004@gmail.com>
2022-10-14 09:18:30 -0700

    puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: char string
+ * Return: printed string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar ('\n');
+		return;
+	}
+}
