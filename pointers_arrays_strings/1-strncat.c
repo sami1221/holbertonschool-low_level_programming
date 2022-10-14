240cfec99f39479a1aa17b28819f643797d91455
sami1221 <samarabts2004@gmail.com>
2022-10-13 17:28:03 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: destiny
+ * @src: source
+ * @n: bytes
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n);
