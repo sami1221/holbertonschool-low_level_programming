f4fc0495b5195e7d9a8ff8f49cfdd45bac295c89
sami1221 <samarabts2004@gmail.com>
2022-10-14 09:04:56 -0700

    mains

---
+++ recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* prototypes*/
+int _putchar (char c);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif /*main.h*/
