e5f83e41ffab2bb42abd8a8b71d69e818563dad1
sami1221 <samarabts2004@gmail.com>
2022-10-14 06:01:59 -0700

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,3 +8,13 @@
  * Return: pointer to dest
  */
 char *_strncat(char *dest, char *src, int n);
+int x, y;
+{
+	for (x = 0; dest[x] != '\0'; x++)
+	for (y = 0; src[y] != '\0'; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+	return (dest);
+}
