3fc3120918fbdaea15de1ba69b2c9b67b5b05b7a
sami1221 <samarabts2004@gmail.com>
2022-10-17 08:13:06 -0700

    strlen recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,10 +4,11 @@
  * @s: pointer
  * Return: length
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
 		return (_strlen_recursion(s + 1) + 1);
 	}
+	return (0);
 }
