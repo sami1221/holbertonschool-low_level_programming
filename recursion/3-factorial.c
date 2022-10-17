35c6bf7c203d9fe0af4da5f5cb957c6aad67e8a0
sami1221 <samarabts2004@gmail.com>
2022-10-17 10:44:05 -0700

    factorial git commit -m factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int factorial- calculates factorial of given n
+ * @n: number
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
