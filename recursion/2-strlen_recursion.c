812c144dab0b6c9e25b23aa0d90623b15be9c03c
sami1221 <samarabts2004@gmail.com>
2022-10-17 08:11:01 -0700

    strlen recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer
+ * Return: length
+ */
+int _strlen_recursion(char *s);
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+}
