75f66e0b3a462f3b873a7c538a95049aa115c54e
sami1221 <samarabts2004@gmail.com>
2022-10-18 09:07:05 -0700

    sqrt recursion1

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,20 +3,20 @@
  * _sqrt_recursion- calculate natural square root of n
  * @n: number
  */
-int _sqrt_recursion(int n);
+int _sqrt_recursion(int n)
 {
 	if (n == 0)
 	{
 		return (0);
 	}
-	_sqrt_recursion2(n, 0);
+	return _sqrt_recursion2(n, 0);
 }
 /**
  * _sqrt_recursion2- aux function
  * @n: number
  * @i: square root of n
  */
-int _sqrt_recursion2(int n, int i);
+int _sqrt_recursion2(int n, int i)
 {
 	if (i * i == n)
 	{
