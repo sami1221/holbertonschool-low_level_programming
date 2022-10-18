1e5a3f18de8efc343eb5588ce21b9bc44745f055
sami1221 <samarabts2004@gmail.com>
2022-10-18 09:47:03 -0700

    sqrt recursion2

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -24,13 +24,9 @@ int _sqrt_recursion2(int n, int i)
 	{
 		return (i);
 	}
-	return (_sqrt_recursion2(n, i + 1));
 	if (i * i > n)
 	{
 		return (-1);
 	}
-	else if (i * i < n)
-	{
-		return (-1);
-	}
+	return (_sqrt_recursion2(n, i + 1));
 }
