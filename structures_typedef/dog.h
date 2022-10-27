9677da7605038ba2e68f3e0b5b23d9679ea54813
sami1221 <samarabts2004@gmail.com>
2022-10-27 08:12:36 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Dogs structure
+ * @name: Dogs name
+ * @age: Dogs age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
