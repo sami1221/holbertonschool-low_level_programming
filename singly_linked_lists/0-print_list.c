bc13914603a325632edcd0b2fe17a85c3a52dff0
sami1221 <samarabts2004@gmail.com>
2022-11-16 08:57:28 -0800

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[%u], %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
